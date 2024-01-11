45d6502759d090f28c982e92a335d1a39189e1c0
ramoni2424 <72168270+ramoni2424@users.noreply.github.com>
2024-01-11 11:54:22 +0100

    .

--- src/main.tsx
+++ src/main.tsx
@@ -2,21 +2,4 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App.tsx'
 
-import React, { useEffect } from 'react';
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
-
-const Componente = () => {
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      window.location.href = 'https://www.ejemplo.com';
-    }, 5000);
-
-    return () => clearTimeout(timer); // Limpiar el temporizador si el componente se desmonta
-  }, []);
-
-  return <div>Redirigiendo a www.ejemplo.com en 5 segundos...</div>;
-};
-
-export default Componente;
-
